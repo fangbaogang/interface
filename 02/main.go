11e0ad9b54561ef5e07b9019e8f34da49ac12364
fangbaogang <402866119@qq.com>
2019-10-21 20:49:07 +0800

    、

--- 02/main.go
+++ 02/main.go
@@ -25,8 +25,9 @@ func create_dog(name string) Sayer {
 func main() {
 	d := create_dog("黑豹")
 	e, ok := d.(*dog)
+	//ok 是 true 表明 Sayer 存储的是 *dog 类型的值
+	//如果需要区分多种类型，可以使用 switch 断言，更简单直接，这种断言方式只能在 switch 语句中使用
 	if ok {
-
 		fmt.Println(e.name)
 		e.say()
 	}
